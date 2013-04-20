a4f4613529ba12dc22d3946de30f37d0ed6bc8a1
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-04-20 13:11:40 +0900

    Build kernel

--- device_f11d.mk
+++ device_f11d.mk
@@ -11,16 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += device/fujitsu/f11d/overlay
 PRODUCT_COPY_FILES += \
   device/fujitsu/f11d/root/ueventd.f11d.rc:root/ueventd.f11d.rc
 
-LOCAL_PATH := device/fujitsu/f11d
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
