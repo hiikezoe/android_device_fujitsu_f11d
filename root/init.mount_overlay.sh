7d8e3608ff5a2a15ce768ff5f271e6226c0b10aa
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-05-12 17:08:05 +0900

    Do not overlay original /data partition

--- root/init.mount_overlay.sh
+++ root/init.mount_overlay.sh
@@ -1,4 +1,3 @@
 #!/system/bin/sh
 
 /system/bin/mount -t overlayfs -olowerdir=/system,upperdir=/b2g overlayfs /system
-/system/bin/mount -t overlayfs -olowerdir=/b2g-data,upperdir=/data overlayfs /data
