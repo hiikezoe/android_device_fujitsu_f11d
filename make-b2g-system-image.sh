1d2e72e5b499c3d6d4024156b70fe65129b022e0
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-05-12 15:17:15 +0900

    Make b2g-system.img

---
+++ make-b2g-system-image.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PRODUCT_OUT=../../../out/target/product/f11d
+B2G_SYSTEM_BASE=$PRODUCT_OUT/b2g-system
+
+mkdir -p $B2G_SYSTEM_BASE
+
+echo "Copying b2g system image files ..."
+for FILE in `cat b2g-files.txt | grep -v ^# | grep -v ^$`; do
+    DEST_DIR=$B2G_SYSTEM_BASE/`dirname $FILE`
+    if [ ! -d $DEST_DIR ]; then
+        mkdir -p $DEST_DIR
+    fi
+    cp -ra $PRODUCT_OUT/system/$FILE $DEST_DIR
+done
+
+rm -f $PRODUCT_OUT/b2g-system.img
+make_ext4fs -l 48M $PRODUCT_OUT/b2g-system.img $B2G_SYSTEM_BASE
